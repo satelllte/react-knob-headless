26acc8bdbd42d3465d265656e408eabdcf63ec43
George Satellite <lunaerxs@gmail.com>
2023-09-24 16:33:13 +0300

    namespaced private packages, rkh draft

---
+++ packages/react-knob-headless/src/index.ts
@@ -0,0 +1,1 @@
+export {example} from './example';
--- packages/react-knob-headless/tsup.config.ts
+++ packages/react-knob-headless/tsup.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
+  treeshake: true,
   legacyOutput: true,
   sourcemap: true,
   clean: true,
