ac3b1434fb2cef04a072f1eafc28e4fc8facd10d
George Satellite <lunaerxs@gmail.com>
2023-09-26 20:25:55 +0300

    page: KnobHeadlessDemo

---
+++ apps/docs/src/components/KnobHeadlessDemo.tsx
@@ -0,0 +1,6 @@
+'use client';
+import {KnobHeadless} from 'react-knob-headless';
+
+export function KnobHeadlessDemo() {
+  return <KnobHeadless valueDefault={0.5} className='h-12 w-8 bg-blue-400' />;
+}
