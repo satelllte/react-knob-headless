993b2fb180faea154cf31b708c78c18177f9658f
George Satellite <lunaerxs@gmail.com>
2023-09-26 22:09:30 +0300

    fix: lint

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 
+// eslint-disable-next-line new-cap
 const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
