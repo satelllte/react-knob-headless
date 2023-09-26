7f69d773c3d640a703308defe2b3cc63d67d6606
Yehor <lunaerxs@gmail.com>
2023-09-26 22:35:00 +0300

    [WIP] Knob (1) (#1)

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,11 +1,11 @@
-import {KnobHeadless} from 'react-knob-headless';
+import {KnobHeadlessDemo} from '../components/KnobHeadlessDemo';
 
 function IndexPage() {
   return (
     <>
       <h1 className='px-4 py-8 text-center text-4xl'>React Knob Headless</h1>
       <div className='flex items-center justify-center bg-stone-800 px-4 py-8'>
-        <KnobHeadless />
+        <KnobHeadlessDemo />
       </div>
     </>
   );
