89b68d8d23fe5e74c0620ca05438457d0070ad07
George Satellite <lunaerxs@gmail.com>
2023-10-08 01:16:29 +0300

    top section: bolder text

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -4,10 +4,10 @@ function IndexPage() {
   return (
     <div className='max-w-3xl px-4 mx-auto'>
       <div className='pt-12 md:pt-16'>
-        <h1 className='text-3xl sm:text-4xl md:text-5xl'>
+        <h1 className='font-bold text-3xl sm:text-4xl md:text-5xl'>
           React Knob Headless
         </h1>
-        <p className='text-xl sm:text-2xl pt-3 text-stone-400'>
+        <p className='font-semibold text-xl sm:text-2xl pt-3 text-stone-400'>
           Unstyled & accessible <b>knob</b> primitive for React.
         </p>
       </div>
