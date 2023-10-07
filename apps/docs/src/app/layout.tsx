aa6be4fe38aab7bde7f6f0b3b1541b4e255ac676
George Satellite <lunaerxs@gmail.com>
2023-10-08 00:38:06 +0300

    cleanup all knobs

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-stone-950 text-stone-100`}>
+      <body className={`${inter.className} bg-stone-950 text-stone-50`}>
         {children}
       </body>
     </html>
