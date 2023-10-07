2ded5646ce67899d9362aeaabcd293638ae34dd5
George Satellite <lunaerxs@gmail.com>
2023-10-08 01:08:10 +0300

    page metadata: update description

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'React Knob Headless',
-  description: 'Headless knob component for React',
+  description: 'Unstyled & accessible knob primitive for React',
 };
 
 export default function RootLayout({
