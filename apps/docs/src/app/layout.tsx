9f05a28f78f0ae8a09a8cd38545f6dca4bee928e
Yehor <lunaerxs@gmail.com>
2023-10-08 18:18:09 +0300

    OpenGraph, Favicon, Apple Icon (#24)

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -5,9 +5,32 @@ import {Inter} from 'next/font/google';
 // eslint-disable-next-line new-cap
 const inter = Inter({subsets: ['latin']});
 
+const title = 'React Knob Headless';
+const description = 'Unstyled & accessible knob primitive for React';
+const images = [
+  {
+    url: '/og_image.png',
+    alt: description,
+    width: 1200,
+    height: 630,
+  },
+];
+
 export const metadata: Metadata = {
-  title: 'React Knob Headless',
-  description: 'Unstyled & accessible knob primitive for React',
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    siteName: title,
+    images,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images,
+  },
 };
 
 export default function RootLayout({
