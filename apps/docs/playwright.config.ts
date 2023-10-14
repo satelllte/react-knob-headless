efcde747343fbc402f4d3216d33107538311428d
George Satellite <lunaerxs@gmail.com>
2023-10-14 15:15:26 +0300

    playwright: remove commented code

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -1,11 +1,5 @@
 import {defineConfig, devices} from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 const port = process.env.PORT ?? 3000;
 const baseUrl = `http://localhost:${port}`;
 
@@ -77,11 +71,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
